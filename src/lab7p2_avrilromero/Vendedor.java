a7684675eaf181eada2432654379cb00c647da19
valeriaaa12 <valeriaromero1275@gmail.com>
2023-09-01 13:48:04 -0600

    Clase vendedor

--- src/lab7p2_avrilromero/Vendedor.java
+++ src/lab7p2_avrilromero/Vendedor.java
@@ -9,5 +9,47 @@ package lab7p2_avrilromero;
  * @author Valeria Romero
  */
 public class Vendedor {
-    
+
+    private String nombre;
+    private int cant_carros;
+    private double dinero;
+
+    public Vendedor() {
+    }
+
+    public Vendedor(String nombre, int cant_carros, double dinero) {
+        this.nombre = nombre;
+        this.cant_carros = cant_carros;
+        this.dinero = dinero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCant_carros() {
+        return cant_carros;
+    }
+
+    public void setCant_carros(int cant_carros) {
+        this.cant_carros = cant_carros;
+    }
+
+    public double getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(double dinero) {
+        this.dinero = dinero;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + "," + "\n" + cant_carros + "," + "\n" + dinero;
+    }
+
 }
