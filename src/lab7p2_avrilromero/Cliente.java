44783531c82c46d56d60728d62bdd097f014a16f
valeriaaa12 <valeriaromero1275@gmail.com>
2023-09-01 13:56:28 -0600

    Clases terminadas

--- src/lab7p2_avrilromero/Cliente.java
+++ src/lab7p2_avrilromero/Cliente.java
@@ -9,5 +9,66 @@ package lab7p2_avrilromero;
  * @author Valeria Romero
  */
 public class Cliente {
+    private String nombre;
+    private int edad;
+    private String profesion;
+    private int cant_carros;
+    private double sueldo;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, int edad, String profesion, int cant_carros, double sueldo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.profesion = profesion;
+        this.cant_carros = cant_carros;
+        this.sueldo = sueldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public int getCant_carros() {
+        return cant_carros;
+    }
+
+    public void setCant_carros(int cant_carros) {
+        this.cant_carros = cant_carros;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return  nombre +","+"\n" + edad +","+"\n" + profesion +","+"\n" + cant_carros +","+"\n" + sueldo;
+    }
     
 }
--- src/lab7p2_avrilromero/Venta.java
+++ src/lab7p2_avrilromero/Venta.java
@@ -9,5 +9,57 @@ package lab7p2_avrilromero;
  * @author Valeria Romero
  */
 public class Venta {
-    
+
+    private String vendedor;
+    private String cliente;
+    private double transaccion;
+    private String carro;
+
+    public Venta() {
+    }
+
+    public Venta(String vendedor, String cliente, double transaccion, String carro) {
+        this.vendedor = vendedor;
+        this.cliente = cliente;
+        this.transaccion = transaccion;
+        this.carro = carro;
+    }
+
+    public String getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(String vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getTransaccion() {
+        return transaccion;
+    }
+
+    public void setTransaccion(double transaccion) {
+        this.transaccion = transaccion;
+    }
+
+    public String getCarro() {
+        return carro;
+    }
+
+    public void setCarro(String carro) {
+        this.carro = carro;
+    }
+
+    @Override
+    public String toString() {
+        return vendedor + "," + "\n" + cliente + "," + "\n" + transaccion + "," + "\n" + carro;
+    }
+
 }
